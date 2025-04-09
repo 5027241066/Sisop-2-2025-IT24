84f395292c910b91e756a6f152f4a17819ff2c00
zenknaki <5027241066@student.its.ac.id>
2025-04-09 20:58:04 +0700

    Commit

---
+++ soal_1/action.c
@@ -0,0 +1,7 @@
+#a
+wget --no-check-certificate 'https://drive.usercontent.google.com/u/0/uc?id=1xFn1OBJUuSdnApDseEczKhtNzyGekauK&export=download' -O Clues.zip
+unzip Clues.zip
+rm Clues.zip
+
+#b
+
